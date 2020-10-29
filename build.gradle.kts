99c30cb7577f3c0f9094f43a67a3e557acd427c5
huangwl <410488219@qq.com>
2020-10-29 18:10:51 +0800

    add compileKotlin config

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,12 @@
 import org.gradle.api.tasks.testing.logging.TestLogEvent.*
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
   kotlin("jvm") version "1.4.10"
 }
 
 group = "com.rainmanhhh.github"
-version = "0.0.2"
+version = "0.0.3"
 
 repositories {
   mavenLocal()
@@ -18,6 +19,9 @@ dependencies {
   implementation(kotlin("stdlib"))
   implementation("com.github.victools:jsonschema-generator:4.16.0")
 }
+
+val compileKotlin: KotlinCompile by tasks
+compileKotlin.kotlinOptions.jvmTarget = "1.8"
 
 tasks.withType<Test> {
   useJUnitPlatform()
