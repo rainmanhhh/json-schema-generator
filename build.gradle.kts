9e99149d6514e2a576b2a191a935e1e4a8a6a3fe
huangwl <410488219@qq.com>
2020-10-29 17:55:48 +0800

    fix gradle test task

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,27 @@
+import org.gradle.api.tasks.testing.logging.TestLogEvent.*
+
 plugins {
-    kotlin("jvm") version "1.4.10"
+  kotlin("jvm") version "1.4.10"
 }
 
 group = "com.rainmanhhh.github"
-version = "0.0.1"
+version = "0.0.2"
 
 repositories {
-    mavenLocal()
-    maven("https://maven.aliyun.com/repository/public")
-    maven("https://jitpack.io")
-    jcenter()
+  mavenLocal()
+  maven("https://maven.aliyun.com/repository/public")
+  maven("https://jitpack.io")
+  jcenter()
 }
 
 dependencies {
-    implementation(kotlin("stdlib"))
-    implementation("com.github.victools:jsonschema-generator:4.16.0")
+  implementation(kotlin("stdlib"))
+  implementation("com.github.victools:jsonschema-generator:4.16.0")
+}
+
+tasks.withType<Test> {
+  useJUnitPlatform()
+  testLogging {
+    events = setOf(PASSED, SKIPPED, FAILED)
+  }
 }
